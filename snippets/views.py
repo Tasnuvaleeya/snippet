8cce3f2bb3e738606d73d53499e09c00f2df0874
Tasnuvaleeya <tasnuva2606@gmail.com>
2017-09-08 01:06:26 +0600

    mixins view

--- snippets/views.py
+++ snippets/views.py
@@ -1,10 +1,44 @@
 from .serializers import SnippetSerializer
 from .models import Snippet
-from rest_framework.views import APIView
-from django.http import Http404
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
+# from rest_framework.views import APIView
+# from django.http import Http404
+# from rest_framework.response import Response
+# from rest_framework import status
 
+
+class SnippetList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
+
+    def post(self,request,*args,**kwargs):
+        return self.create(request,*args,**kwargs)
+
+
+class SnippetDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self,request,*args,**kwargs):
+        return self.retrieve(request,*args,**kwargs)
+
+    def put(self,request,*args,**kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self,request,*args,**kwargs):
+        return self.destroy(request,*args,**kwargs)
+
+
+'''
 class SnippetList(APIView):
     def get(self,request,format=None):
         snippet = Snippet.objects.all()
@@ -45,7 +79,4 @@ class SnippetDetail(APIView):
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-
-
-
-
+'''
