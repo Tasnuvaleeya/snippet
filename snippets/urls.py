b9152ba7626ec488e49a1de9f3de35b1ec6459dc
Tasnuvaleeya <tasnuva2606@gmail.com>
2017-09-08 13:30:46 +0600

    updated with authentication

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,10 +1,16 @@
-from django.conf.urls import url
-from .views import SnippetList,SnippetDetail
+from django.conf.urls import url,include
+from .views import SnippetList,SnippetDetail,UserList,UserDetail
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^snippets/$',SnippetList.as_view()),
     url(r'^snippets/(?P<pk>\d+)/$', SnippetDetail.as_view()),
+    url(r'^users/$', UserList.as_view()),
+    url(r'^users/(?P<pk>\d+)/$', UserDetail.as_view()),
 
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns +=[
+    url(r'^api-auth/',include('rest_framework.urls',namespace='rest_framework')),
+]
